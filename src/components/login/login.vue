17eca3bbe4fa6ccfc6669fb87e5958413f87151d
chaiqingshan <1324315849@qq.com>
2020-02-15 16:19:23 +0800

    添加默认登陆用户

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -42,8 +42,8 @@
     export default {
         data() {
             return {
-                userCode: '',
-                password: '',
+                userCode: 'chai',
+                password: '123',
                 data: {},
             }
         },
